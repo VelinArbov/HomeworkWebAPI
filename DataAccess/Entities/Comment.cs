5736b994b4008e9027111352ee72d6ec1d0b9abc
Velin Arbov <48315855+VelinArbov@users.noreply.github.com>
2021-05-22 17:15:48 +0300

    TestCommit

---
+++ DataAccess/Entities/Comment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EndavaWebAPI.DataAccess.Entities
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+
+        //public AppUser? Author { get; set; }
+
+        public DateTime CreatedOn => DateTime.UtcNow;
+
+        public Book? Book { get; set; }
+
+        public bool IsApproved { get; set; }
+    }
+}
